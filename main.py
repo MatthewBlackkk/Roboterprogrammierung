9c33c86597e07c8cd54aeb384df87967ccd84914
Matthias <schwarz.matthias99@gmail.com>
2025-12-09 13:20:19 +0100

    tets

--- main.py
+++ main.py
@@ -13,4 +13,7 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
 
+
+''' Git hub test commit push and pull'''
+
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
