cd74e1b1ae466d6e39db9d205a848d5f4795dc42
ilham-9-9 <ilham.hank9@gmail.com>
2025-12-22 11:05:20 +0100

    discretization with x and y

--- A-Star_Erweiterung/notebooks/IPAStar.py
+++ A-Star_Erweiterung/notebooks/IPAStar.py
@@ -34,10 +34,13 @@ class AStar(PlanerBase):
 
         self.limits = self._collisionChecker.getEnvironmentLimits()
 
-        self.num_steps=44
-        self.step_size=[]
-        for limit in self.limits:
-            self.step_size.append( (limit[1]-limit[0]) / self.num_steps )
+        self.num_steps=[44,44]
+        if len(self.num_steps) != self.dim:
+                raise ValueError(f"Erreur: num_steps must have {self.dim} values (one per dimension).")
+        else:
+            self.step_size=[]
+            for i, limit in enumerate(self.limits):
+                self.step_size.append( (limit[1]-limit[0]) / self.num_steps[i])
 
         self.w = 0.5  
         return
