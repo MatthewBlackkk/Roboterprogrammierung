ad95c7f9d8f60cb28b9916e7c7bc67c3c54d287f
behringerlu <behrlu01@thu.de>
2026-01-07 17:41:37 +0100

    Shape-Roboter-Animation vervollständigt

--- A-Star_Erweiterung/notebooks/IPTestSuite2.py
+++ A-Star_Erweiterung/notebooks/IPTestSuite2.py
@@ -19,6 +19,10 @@ benchList = list()
 robot_shape = Polygon([(-0.5, -0.5), (2, -0.5), (2.0, 0.5), (-0.5, 0.5)])
 shape_robot = ShapeRobotWithOrientation(robot_shape, limits=[[0.0, 22.0,], [0.0, 22.0], [-math.pi, math.pi]])
 
+# Neuer komplexer Roboter (z.B. L-förmig)
+robot_shape_complex = Polygon([(0, 0), (2, 0), (2, 0.5), (0.5, 0.5), (0.5, 2), (0, 2)])
+shape_robot_complex = ShapeRobotWithOrientation(robot_shape_complex, limits=[[0.0, 22.0,], [0.0, 22.0], [-math.pi, math.pi]])
+
 # -----------------------------------------
 trapField = dict()
 trapField["obs1"] =   LineString([(6, 18), (6, 8), (16, 8), (16,18)]).buffer(1.0)
@@ -27,10 +31,10 @@ benchList.append(Benchmark("Trap", CollisionCheckerShapeRobot(trapField, shape_r
 
 # -----------------------------------------
 bottleNeckField = dict()
-bottleNeckField["obs1"] = LineString([(0, 13), (11, 13)]).buffer(.5)
-bottleNeckField["obs2"] = LineString([(13, 13), (23,13)]).buffer(.5)
+bottleNeckField["obs1"] = LineString([(0, 13), (10.5, 13)]).buffer(.5)  # Ende bei 10 statt 11
+bottleNeckField["obs2"] = LineString([(13.5, 13), (23,13)]).buffer(.5)  # Start bei 14 statt 13
 description = "Planer has to find a narrow passage."
-#benchList.append(Benchmark("Bottleneck", CollisionCheckerShapeRobot(bottleNeckField, shape_robot), [[4,15,0]], [[18,1,math.pi/2]], description, 2))
+benchList.append(Benchmark("Bottleneck", CollisionCheckerShapeRobot(bottleNeckField, shape_robot), [[4,15,0]], [[20,10,math.pi/2]], description, 2))
 
 # -----------------------------------------
 fatBottleNeckField = dict()
@@ -55,3 +59,16 @@ myField["Antenna_Head_R"] = Point(8.5, 16).buffer(1)
 myField["Rob_Head"] = Polygon([(2, 13), (2, 8), (8, 8), (8, 13)])
 description = "Planer has to find a passage past a robot head and the print of the LTC."
 #benchList.append(Benchmark("MyField", CollisionCheckerShapeRobot(myField, shape_robot), [[4,21,0]], [[18,1,math.pi/2]], description, 2))
+
+# Trap mit komplexem Roboter
+trapField_complex = dict()
+trapField_complex["obs1"] = LineString([(6, 18), (6, 8), (16, 8), (16,18)]).buffer(1.0)
+description_complex = "Trap scenario with complex L-shaped robot."
+benchList.append(Benchmark("Trap_Complex", CollisionCheckerShapeRobot(trapField_complex, shape_robot_complex), [[10,15,0]], [[20,1,math.pi/2]], description_complex, 2))
+
+# Bottleneck mit komplexem Roboter
+bottleNeckField_complex = dict()
+bottleNeckField_complex["obs1"] = LineString([(0, 13), (10.5, 13)]).buffer(.5)
+bottleNeckField_complex["obs2"] = LineString([(13.5, 13), (23,13)]).buffer(.5)
+description_bottleneck = "Bottleneck with complex L-shaped robot."
+benchList.append(Benchmark("Bottleneck_Complex", CollisionCheckerShapeRobot(bottleNeckField_complex, shape_robot_complex), [[4,15,0]], [[18,1,math.pi/2]], description_bottleneck, 2))
